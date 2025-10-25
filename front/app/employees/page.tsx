a298690880b8327b378a7b3276e3e9d7b48e240f
antoniogeneris <antonio.r@generis.mg>
2025-10-25 23:26:13 +0300

    init side bar and contains

---
+++ front/app/employees/page.tsx
@@ -0,0 +1,8 @@
+export default function EmployeesPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4">Employees</h1>
+      <p>List of employees and their performance scores.</p>
+    </div>
+  );
+}
