b13c00fb8b515423521b1e8b1e74424c89696996
antoniogeneris <antonio.r@generis.mg>
2025-10-30 22:16:10 +0300

    fix error on seed

--- backend/database/seeders/core/User.ts
+++ backend/database/seeders/core/User.ts
@@ -1,11 +1,13 @@
-import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
-import User from "App/Models/User";
-import Hash from "@ioc:Adonis/Core/Hash";
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+import Hash from '@ioc:Adonis/Core/Hash'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class UserSeeder extends BaseSeeder {
   public async run() {
-    await User.query().delete();
-    
+    await User.query().delete()
+    await Database.rawQuery('ALTER SEQUENCE users_id_seq RESTART WITH 1')
+
     await User.createMany([
       {
         firstname: "Alice",
