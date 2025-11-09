b99849b11ac198383de0b7ef077d150b4f651527
antoniogeneris <antonio.r@generis.mg>
2025-11-09 22:58:35 +0300

    layout

--- frontend/app/(protected)/layout.tsx
+++ frontend/app/(protected)/layout.tsx
@@ -1,3 +1,4 @@
+// frontend/app/(protected)/layout.tsx
 "use client";
 
 import { useState } from "react";
@@ -13,17 +14,19 @@ export default function MainLayout({
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
-    <div className="flex h-screen overflow-hidden">
-      {/* Sidebar fixe */}
+    <div className="flex h-screen overflow-hidden bg-gray-100">
+      {/* Sidebar */}
       <Sidebar isCollapsed={isCollapsed} />
 
       {/* Contenu principal */}
       <div className="flex flex-col flex-1 overflow-hidden">
         <Topbar onToggleSidebar={() => setIsCollapsed(!isCollapsed)} />
 
-        <main className="flex-1 overflow-y-auto bg-gray-50">
-          <div className="p-2">
-            <QueryProvider>{children}</QueryProvider>
+        <main className="flex-1 overflow-y-auto">
+          <div className="p-6">
+            <div className="bg-white shadow-md rounded-lg p-6 min-h-[85vh]">
+              <QueryProvider>{children}</QueryProvider>
+            </div>
           </div>
         </main>
       </div>
